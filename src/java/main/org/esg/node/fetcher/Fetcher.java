789b5cc28490124923eb3230828e4834a6dd14cf
dinorahcr <dcarrion@uci.edu>
2012-07-13 11:29:26 -0700

    Cosmetic: added header and indented region etc...

--- src/java/main/org/esg/node/fetcher/Fetcher.java
+++ src/java/main/org/esg/node/fetcher/Fetcher.java
@@ -1,3 +1,52 @@
+/***************************************************************************
+*                                                                          *
+*  Organization: Earth System Grid Federation                              *
+*                                                                          *
+****************************************************************************
+*                                                                          *
+*   Copyright (c) 2009, Lawrence Livermore National Security, LLC.         *
+*   Produced at the Lawrence Livermore National Laboratory                 *
+*   LLNL-CODE-420962                                                       *
+*                                                                          *
+*   All rights reserved. This file is part of the:                         *
+*   Earth System Grid Federation (ESGF)                                    *
+*   Data Node Software Stack, Version 1.0                                  *
+*                                                                          *
+*   For details, see http://esgf.org/esg-node-site/                        *
+*   Please also read this link                                             *
+*    http://esgf.org/LICENSE                                               *
+*                                                                          *
+*   * Redistribution and use in source and binary forms, with or           *
+*   without modification, are permitted provided that the following        *
+*   conditions are met:                                                    *
+*                                                                          *
+*   * Redistributions of source code must retain the above copyright       *
+*   notice, this list of conditions and the disclaimer below.              *
+*                                                                          *
+*   * Redistributions in binary form must reproduce the above copyright    *
+*   notice, this list of conditions and the disclaimer (as noted below)    *
+*   in the documentation and/or other materials provided with the          *
+*   distribution.                                                          *
+*                                                                          *
+*   Neither the name of the LLNS/LLNL nor the names of its contributors    *
+*   may be used to endorse or promote products derived from this           *
+*   software without specific prior written permission.                    *
+*                                                                          *
+*   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS    *
+*   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT      *
+*   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS      *
+*   FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE    *
+*   LIVERMORE NATIONAL SECURITY, LLC, THE U.S. DEPARTMENT OF ENERGY OR     *
+*   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,           *
+*   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT       *
+*   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF       *
+*   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND    *
+*   ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,     *
+*   OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT     *
+*   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF     *
+*   SUCH DAMAGE.                                                           *
+*                                                                          *
+***************************************************************************/
 package org.esg.node.fetcher;
 
 import org.apache.commons.httpclient.*;
@@ -12,7 +61,11 @@ import java.nio.*;
 import java.nio.channels.spi;
 
 
-public static class Fetcher{
+/**
+   Description:
+   This class pulls files from desginated targets to local host
+*/
+public static class Fetcher {
 
     private static File file;
     private URL url;
@@ -21,28 +74,7 @@ public static class Fetcher{
     Path directory;
     String fileName;
 
-    public static void main(String [ ] args){
-
-	if(args.length != 3){
-	    System.err.println("Please enter 3 arguments: url,retry number, and the directory to save");
-	    System.exit(1);
-	}
-		
-	String address = args[0];
-        try{int numRetries = Integer.parseInt(args[1]);}
-	catch (NumberFormatException e) {
-        System.err.println("The third argument" + " must be an integer");
-        System.exit(1);
-	}
-	
-	directory = args[2];
-	client = new HttpClient();
-	fetchURLContent(address, numRetries);
-	setDirectory(args[2]);
-
-    }
-
-    public void fetchURLContent(String address, int retries){
+    public void fetchURLContent(String address, int retries) {
 	
 	url = new URL(address);
         isValid(); //check this method. it might be unecessary! 
@@ -50,21 +82,21 @@ public static class Fetcher{
 	retry = retries;
 	HttpMethod getHttp = new GetMethod(url);
 	getHttp.setFollowRedirects(true);
-	HttpMethodRetryHandler retryHandler = new HttpMethodRetryHandler(){retryMethod};
+	HttpMethodRetryHandler retryHandler = new HttpMethodRetryHandler() { retryMethod };
         getHttp.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, retryHandler);
 
 	try{
-	     int codeStatus =  client.executeMethod(getHttp);
-	     if(statusCode != HttpStatus.SC_OK){System.err.println("Method failed: " + getHttp.getStatusLine().toString());}
-	     else{System.out.prinln("Success");}
-	     /*InputStream in = getHttp.getResponseBodyAdStream();
-	     FileOutPutStream out = new FileOutputStream(setDirectory);
-	     int count = -1;
-	     while((count = in.read(buffer)) != -1){
-		 out.write(buffer, 0, count);
-	     }
-	     out.flush();
-	     out.close(); */
+	    int codeStatus =  client.executeMethod(getHttp);
+	    if(statusCode != HttpStatus.SC_OK){ System.err.println("Method failed: " + getHttp.getStatusLine().toString()); }
+	    else{System.out.prinln("Success");}
+	    /*InputStream in = getHttp.getResponseBodyAdStream();
+	      FileOutPutStream out = new FileOutputStream(setDirectory);
+	      int count = -1;
+	      while((count = in.read(buffer)) != -1){
+	      out.write(buffer, 0, count);
+	      }
+	      out.flush();
+	      out.close(); */
 	}	
 
 	catch(HttpException e){
@@ -77,23 +109,23 @@ public static class Fetcher{
 	byte[] responseBody = getUrl.getResponseBodyAsStream();
 	
 	try(SeekByteChannel sbc = Files.newByteChannel(responseBody)){
-	ByteByffer buffer = ByteBuffer.allocate(1024);	
-	String encoding = System.getProperty("file.ecoding");
+		ByteByffer buffer = ByteBuffer.allocate(1024);	
+		String encoding = System.getProperty("file.ecoding");
 	   
-	  while(sbc.read(buffer)){
-	    buffer.rewind();
-	    System.out.print(Charset.forName(encoding).decode(buffer)); //gets name of the charset,Changes to Unicorde
-	    buffer.flip();
-       	  }
-	}
+		while(sbc.read(buffer)){
+		    buffer.rewind();
+		    System.out.print(Charset.forName(encoding).decode(buffer)); //gets name of the charset,Changes to Unicorde
+		    buffer.flip();
+		}
+	    }
 
 	catch(ClosedChannelException e){
 	    System.err.println(e.getMessage);
 	    e.printStackTrace();
 	}
 	catch(AsynchronousCloseException e){
-	   System.err.println(e.getMessage);
-	   e.printStackTrace();
+	    System.err.println(e.getMessage);
+	    e.printStackTrace();
 	}
 	catch(ClosedByInterruptException e){
 	    System.err.println(e.getMessage);
@@ -109,7 +141,7 @@ public static class Fetcher{
     }
 
 
-    public boolean retryMethod(final HttpMethod method, final IOException, int retryCount){
+    public boolean retryMethod(final HttpMethod method, final IOException, int retryCount) {
 
 	if(retryCount >= retry){ return false; }
 	if(exception instanceof NoHttpResponseException){ return true; }
@@ -120,28 +152,28 @@ public static class Fetcher{
     }
 
     
-    public boolean setDirectory(String dir){
+    public boolean setDirectory(String dir) {
 	filename = url.getFile();   
 	return file(url, new File(filename));
 	/*	File currentDir = System.getProperty("user.dir");
-	File dirc = new File(dir, filename);
-	if(!dirc.exists){dirc.createNewFile();)
+		File dirc = new File(dir, filename);
+		if(!dirc.exists){dirc.createNewFile();)
 	*/
-       }
+    }
 	
-    public String  getUrlInfo(){
+    public String  getUrlInfo() {
 	Path path = url.getPath;
 	String fileN = url.getFile();
 	return System.out.println("FileName: " + fileN + "Path: " + path);
 	
     }
 
-    public boolean validateUrl(){
+    public boolean validateUrl() {
 	boolean isValid = false;
 	String[] schemes = {"http", "https"};
 	UrlValidator urlValidator = new UrlValidator(schemes);
 	
-	if(!UrlValidator.isValid(url)){
+	if(!UrlValidator.isValid(url)) {
 	    System.out.print("Url was invalid. Please try again");
             isValid = false;
 	}
@@ -152,6 +184,25 @@ public static class Fetcher{
 	return isValid;
 
     }
+
+    public static void main(String [ ] args) {
+	if(args.length != 3) {
+	    System.err.println("Please enter 3 arguments: url,retry number, and the directory to save");
+	    System.exit(1);
+	}
+		
+	String address = args[0];
+        try{int numRetries = Integer.parseInt(args[1]);}
+	catch (NumberFormatException e) {
+	    System.err.println("The third argument" + " must be an integer");
+	    System.exit(1);
+	}
+	
+	directory = args[2];
+	client = new HttpClient();
+	fetchURLContent(address, numRetries);
+	setDirectory(args[2]);
+
     }
 
 }
